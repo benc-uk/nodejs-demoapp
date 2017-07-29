66cb861a1365c17701521b35d047b67201562e77
Ben Coleman <benc.uk@gmail.com>
2017-07-29 23:12:42 +0100

    Not needed

--- build.ps1
+++
@@ -1,5 +0,0 @@
-npm install
-docker build . -t bencuk/nodejs-demoapp
-docker build . -f Dockerfile.node6 -t bencuk/nodejs-demoapp:node6
-docker push bencuk/nodejs-demoapp
-docker push bencuk/nodejs-demoapp:node6
