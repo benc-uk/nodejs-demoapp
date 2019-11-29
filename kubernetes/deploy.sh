dc08f44f899433030e099d64b6c00e54e83f0d38
Ben Coleman <benc.uk@gmail.com>
2019-11-29 13:17:20 +0000

    yaml shuffle

--- kubernetes/deploy.sh
+++
@@ -1,6 +0,0 @@
-#helm upgrade db https://kubernetes-charts.storage.googleapis.com/mongodb-7.4.6.tgz -f db.yaml -n demoapps --install
-
-#sleep 20
-
-helm upgrade nodejs https://code.benco.io/helm-charts/webapp-1.0.0.tgz -f app.yaml -n demoapps --install
-
