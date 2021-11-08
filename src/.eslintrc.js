a3236af2e5eb6bfeb3c20c95d775f949146abeea
Ben Coleman <benc.uk@gmail.com>
2021-11-08 17:53:28 +0000

    eslint simplify

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -5,29 +5,20 @@ module.exports = {
     es6: true,
     mocha: true,
     jquery: true,
-    browser: true
+    browser: true,
   },
-  extends: [
-    'eslint:recommended'
-  ],
+  extends: ['eslint:recommended'],
   rules: {
     // Errors & best practices
     'no-var': 'error',
     'no-console': 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-unused-vars': ['error', { 'argsIgnorePattern': 'next|res|req' }],
-    'curly': 'error',
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next|res|req' }],
+    curly: 'error',
 
     // ES6
     'arrow-spacing': 'error',
     'arrow-parens': 'error',
-
-    // Style
-    'brace-style': ['error', '1tbs', { 'allowSingleLine': true } ],
-    'semi': ['error', 'never'],
-    'indent': ['error', 2],
-    'quotes': ['error', 'single'],
-    'camelcase': 'error',
 
     // Spacing
     'func-call-spacing': 'error',
@@ -36,9 +27,9 @@ module.exports = {
     'no-trailing-spaces': 'error',
     'no-whitespace-before-property': 'error',
     'comma-spacing': 'error',
-    'keyword-spacing':  'error',
+    'keyword-spacing': 'error',
   },
   parserOptions: {
-    'ecmaVersion': '2017'
-  }
+    ecmaVersion: '2017',
+  },
 }
