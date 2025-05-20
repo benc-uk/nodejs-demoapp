5ee7187b5f0213ff599931f00e81063d7fa067f2
Daniil Tsitavets <d.tsitavets@gmail.com>
2025-05-20 17:37:03 +0300

    refactor: add associate_public_ip_address true value to ec2 template

--- weak3_task1/infrastructure/common/ec2.tf
+++ weak3_task1/infrastructure/common/ec2.tf
@@ -3,9 +3,12 @@ resource "aws_launch_template" "demo_lt" {
   image_id      = "ami-0c1ac8a41498c1a9c"
   instance_type = "t3.micro"
 
-  vpc_security_group_ids = [aws_security_group.demo_ec2_sg.id]
-
   user_data = base64encode(file("../../user_data/user-data.sh"))
+
+  network_interfaces {
+    associate_public_ip_address = true
+    security_groups = [aws_security_group.demo_ec2_sg.id]
+  }
 
   lifecycle {
     create_before_destroy = true
